26729c795d0422e9c9c9a2ce76f2da49c65bd539
Eric Romano <ecr1980@gmail.com>
2022-03-15 10:36:51 -0400

    added a valid move method

--- lib/rook.rb
+++ lib/rook.rb
@@ -10,4 +10,68 @@ class Rook
     end
   end
 
+  def valid_moves
+    valid_moves = Array.new()
+    can_move = true
+    until can_move == false
+      change = 1
+      if position[0]+change > 7
+        can_move = false
+      elsif game_board[(position[0]+change),position[1]].piece == nil
+        valid_moves << game_board[(position[0]+change),position[1]]
+        change += 1
+      elsif game_board[(position[0]+change),position[1]].piece.player != @player
+        valid_moves << game_board[(position[0]+change),position[1]]
+        change += 8
+      else
+        can_move = false
+      end
+    end
+    can_move = true
+    until can_move == false
+      change = -1
+      if position[0]+change < 0
+        can_move = false
+      elsif game_board[(position[0]+change),position[1]].piece == nil
+        valid_moves << game_board[(position[0]+change),position[1]]
+        change -= 1
+      elsif game_board[(position[0]+change),position[1]].piece.player != @player
+        valid_moves << game_board[(position[0]+change),position[1]]
+        change -= 8
+      else
+        can_move = false
+      end
+      can_move = true
+    until can_move == false
+      change = 1
+      if position[0]+change > 7
+        can_move = false
+      elsif game_board[position[0],(position[1]+change)].piece == nil
+        valid_moves << game_board[position[0],(position[1]+change)]
+        change += 1
+      elsif game_board[position[0],(position[1]+change)].piece.player != @player
+        valid_moves << game_board[position[0],(position[1]+change)]
+        change += 8
+      else
+        can_move = false
+      end
+    end
+    can_move = true
+    until can_move == false
+      change = -1
+      if position[0]+change < 0
+        can_move = false
+      elsif game_board[position[0],(position[1]+change)].piece == nil
+        valid_moves << game_board[position[0],(position[1]+change)]
+        change -= 1
+      elsif game_board[position[0],(position[1]+change)].piece.player != @player
+        valid_moves << game_board[position[0],(position[1]+change)]
+        change -= 8
+      else
+        can_move = false
+      end
+    end
+
+
+
 end
