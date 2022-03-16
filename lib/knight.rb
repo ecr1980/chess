5b7af34d9645b34b8bb218a13c40d3f4468ead61
Eric Romano <ecr1980@gmail.com>
2022-03-16 13:36:22 -0400

    Has valid movements

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,3 +1,5 @@
+#require './chess'
+
 class Knight
   attr_accessor :player, :position, :token
   def initialize(player, position)
@@ -10,4 +12,27 @@ class Knight
     end
   end
 
+  def valid_moves(game_board)
+    valid_moves = Array.new()
+    can_move = [[position[0]+1,position[1]+2],
+                [position[0]+1,position[1]-2],
+                [position[0]-1,position[1]+2],
+                [position[0]-1,position[1]-2],
+                [position[0]+2,position[1]+1],
+                [position[0]+2,position[1]-1],
+                [position[0]-2,position[1]+1],
+                [position[0]-2,position[1]-1]]
+    8.times do |index|
+      if can_move[index][0] < 8 && can_move[index][0] >= 0
+        if can_move[index][1] < 8 && can_move[index][1] >= 0
+           if game_board[can_move[index][0]][can_move[index][1]].piece == nil
+            valid_moves << can_move[index]
+           elsif game_board[can_move[index][0]][can_move[index][1]].piece.player != @player
+            valid_moves << can_move[index]
+          end
+        end
+      end
+    end
+    return valid_moves
+  end
 end
